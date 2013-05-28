bb0b654893d6ed32b58934b28fc70f780c586bd8
saikiranmothe <saikiran.mothe@gmail.com>
2013-05-28 14:28:58 +0530

    super admin added in Intializers

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -5,7 +5,16 @@ ActiveAdmin.setup do |config|
   # Set the title that is displayed on the main layout
   # for each of the active admin pages.
   #
-  config.site_title = "Mj"
+  config.site_title = "Maheshwari Jagruthi Admin"
+
+
+    config.namespace :admin do |admin|
+      admin.site_title = "Admin Site"
+    end
+
+    config.namespace :super_admin do |super_admin|
+      super_admin.site_title = "Super Admin Site"
+    end
 
   # Set the link url for the title. For example, to take
   # users to your main site. Defaults to no link.
